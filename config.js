a97631420e767866632eae7362f471bfcdbb1380
DEMONE01 <nafissatounsd@gmail.com>
2024-12-06 23:46:01 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "-",
+SESSION_ID: process.env.SESSION_ID || "CEBQhBqb#a3i7nNLaEqtrZ92HTEp0J8wskMY9S3mSPWFmyvMt46M",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
